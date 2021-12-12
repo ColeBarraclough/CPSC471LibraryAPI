c7d56f7e35cd17b13438d09613112547577af2dc
ColeB <butueth@gmail.com>
2021-12-12 01:39:45 -0700

    Add project files.

---
+++ DatabaseLibrary/Helpers/CustomerHelper_db.cs
@@ -0,0 +1,117 @@
+﻿using DatabaseLibrary.Core;
+using DatabaseLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Net;
+using System.Text;
+
+namespace DatabaseLibrary.Helpers
+{
+    public class CustomerHelper_db
+    {
+
+        /// <summary>
+        /// Adds a new instance into the database.
+        /// </summary>
+        public static Customer_db Add(string firstName, string lastName, string password, string address, DateTime dateOfBirth,
+            DbContext context, out StatusResponse statusResponse)
+        {
+            try
+            {
+                // Validate
+                if (string.IsNullOrEmpty(firstName?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a first name.");
+                if (string.IsNullOrEmpty(lastName?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a last name.");
+                if (string.IsNullOrEmpty(password?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a password.");
+                if (string.IsNullOrEmpty(address?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a address.");
+                if (dateOfBirth == default(DateTime))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a date of birth.");
+
+                // Generate a new instance
+                Customer_db instance = new Customer_db
+                    (
+                        cardId: 0, //This can be ignored is PK in your DB is auto increment
+                        firstName, lastName, password, address, dateOfBirth
+                    );
+
+                // Add to database
+                int rowsAffected = context.ExecuteNonQueryCommand
+                    (
+                        commandText: "INSERT INTO customer (First_name, Last_name, Password, Address, Date_of_birth) values (@card_id, @first_name, @last_name, @password, @address, @date_of_birth)",
+                        parameters: new Dictionary<string, object>()
+                        {
+                            { "@first_name", instance.FirstName },
+                            { "@last_name", instance.LastName },
+                            { "@password", instance.Password },
+                            { "@address", instance.Address },
+                            { "@date_of_birth", instance.DateOfBirth }
+                        },
+                        message: out string message
+                    );
+                if (rowsAffected == -1)
+                    throw new Exception(message);
+
+                // Return value
+                statusResponse = new StatusResponse("Student added successfully");
+                return instance;
+            }
+            catch (Exception exception)
+            {
+                statusResponse = new StatusResponse(exception);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves a list of instances.
+        /// </summary>
+        public static List<Customer_db> GetCollection(
+            DbContext context, out StatusResponse statusResponse)
+        {
+            try
+            {
+                // Get from database
+                DataTable table = context.ExecuteDataQueryCommand
+                    (
+                        commandText: "SELECT * FROM customer",
+                        parameters: new Dictionary<string, object>()
+                        {
+
+                        },
+                        message: out string message
+                    );
+                if (table == null)
+                    throw new Exception(message);
+
+                // Parse data
+                List<Customer_db> instances = new List<Customer_db>();
+                foreach (DataRow row in table.Rows)
+                    instances.Add(new Customer_db
+                            (
+                                cardId: (int) row["Card_id"],
+                                firstName: row["First_name"].ToString(), 
+                                lastName: row["Last_name"].ToString(),
+                                password: row["Password"].ToString(),
+                                address: row["Address"].ToString(),
+                                dateOfBirth: (DateTime) row["Date_of_birth"]
+                            )
+                        );
+
+                // Return value
+                statusResponse = new StatusResponse("Customer list has been retrieved successfully.");
+                return instances;
+            }
+            catch (Exception exception)
+            {
+                statusResponse = new StatusResponse(exception);
+                return null;
+            }
+        }
+
+    }
+}
---
+++ Webservice/ControllerHelpers/CustomerHelper.cs
@@ -0,0 +1,95 @@
+﻿using BusinessLibrary.Models;
+using DatabaseLibrary.Core;
+using DatabaseLibrary.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Webservice.ControllerHelpers
+{
+    public class CustomerHelper
+    {
+
+        #region Converters
+
+        /// <summary>
+        /// Converts database models to a business logic object.
+        /// </summary>
+        public static BusinessLibrary.Models.Customer Convert(Customer_db instance)
+        {
+            if (instance == null)
+                return null;
+            return new BusinessLibrary.Models.Customer(instance.CardId, instance.FirstName, instance.LastName, instance.Password, instance.Address, instance.DateOfBirth);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Signs up a customer.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage Add(JObject data,
+            DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Extract paramters
+            string firstName = (data.ContainsKey("firstName")) ? data.GetValue("firstName").Value<string>() : null;
+            string lastName = (data.ContainsKey("lastName")) ? data.GetValue("lastName").Value<string>() : null;
+            string password = (data.ContainsKey("password")) ? data.GetValue("password").Value<string>() : null;
+            string address = (data.ContainsKey("address")) ? data.GetValue("address").Value<string>() : null;
+            DateTime dateOfBirth = (data.ContainsKey("date_of_birth")) ? data.GetValue("date_of_birth").Value<DateTime>() : default(DateTime);
+
+            // Add instance to database
+            var dbInstance = DatabaseLibrary.Helpers.CustomerHelper_db.Add(firstName, lastName, password, address, dateOfBirth,
+                context, out StatusResponse statusResponse);
+
+            // Get rid of detailed internal server error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors)
+                statusResponse.Message = "Something went wrong while adding a new customer.";
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    dbInstance != null,
+                    statusResponse.Message,
+                    Convert(dbInstance)
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+        }
+
+        /// <summary>
+        /// Gets list of students.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage GetCollection(
+            DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Get instances from database
+            var dbInstances = DatabaseLibrary.Helpers.CustomerHelper_db.GetCollection(
+                context, out StatusResponse statusResponse);
+
+            // Convert to business logic objects
+            var instances = dbInstances?.Select(x => Convert(x)).ToList();
+
+            // Get rid of detailed error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors)
+                statusResponse.Message = "Something went wrong while retrieving the customers";
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    instances != null,
+                    statusResponse.Message,
+                    instances
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+        }
+
+    }
+}
