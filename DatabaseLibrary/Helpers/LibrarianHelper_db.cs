a43aa5d06dc3cad1ef2b69acdcb65ea133e0b252
ParkerJSmith <parker.smith@shaw.ca>
2021-12-16 00:21:34 -0700

    Librarian added

---
+++ DatabaseLibrary/Helpers/LibrarianHelper_db.cs
@@ -0,0 +1,281 @@
+﻿using DatabaseLibrary.Core;
+using DatabaseLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Net;
+using System.Text;
+
+namespace DatabaseLibrary.Helpers
+{
+    public class LibrarianHelper_db
+    {
+
+        /// <summary>
+        /// Adds a new instance into the database.
+        /// </summary>
+        public static Librarian_db Add(string employee_id, string firstName, string lastName, string phone_no, string address, string social_insurance_no, string library_address,
+            DbContext context, out StatusResponse statusResponse)
+        {
+            try
+            {
+                // Validate
+                if (string.IsNullOrEmpty(firstName?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a first name.");
+                if (string.IsNullOrEmpty(lastName?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a last name.");
+                if (string.IsNullOrEmpty(phone_no?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a phone number.");
+                if (string.IsNullOrEmpty(address?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a address.");
+                if (string.IsNullOrEmpty(social_insurance_no?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a social insurance number.");
+                if (string.IsNullOrEmpty(social_insurance_no?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a social insurance number.");
+
+                // Generate a new instance
+                Librarian_db instance = new Librarian_db
+                    (
+                        employee_id, firstName, lastName, phone_no, address, social_insurance_no, library_address
+                    );
+
+                // Add to database
+                int rowsAffected = context.ExecuteNonQueryCommand
+                    (
+                        commandText: "INSERT INTO librarian (First_name, Last_name, Password, Address, Date_of_birth) values (@first_name, @last_name, @phone_no, @address, @date_of_birth)",
+                        parameters: new Dictionary<string, object>()
+                        {
+                            { "@first_name", instance.First_name },
+                            { "@last_name", instance.Last_name },
+                            { "@phone_no", instance.Phone_no },
+                            { "@address", instance.Address },
+                            { "@social_insurance_no", instance.Social_insurance_no },
+                            { "@library_address", instance.Library_address }
+                        },
+                        message: out string message
+                    );
+                if (rowsAffected == -1)
+                    throw new Exception(message);
+
+                // Get from database
+                DataTable table = context.ExecuteDataQueryCommand
+                    (
+                        commandText: "SELECT LAST_INSERT_ID();",
+                        parameters: new Dictionary<string, object>()
+                        {
+
+                        },
+                        message: out string getMessage
+                    );
+                if (table == null)
+                    throw new Exception(getMessage);
+
+                DataRow row = table.Rows[0];
+
+                //instance.Employee_id;
+
+                // Return value
+                statusResponse = new StatusResponse("Customer added successfully");
+                return instance;
+            }
+            catch (Exception exception)
+            {
+                statusResponse = new StatusResponse(exception);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Edits an instance in the database
+        /// </summary>
+        public static Librarian_db Edit(string employee_id, string firstName, string lastName, string phone_no, string address, string social_insurance_no, string library_address,
+           DbContext context, out StatusResponse statusResponse)
+        {
+            try
+            {
+                // Validate
+                if (string.IsNullOrEmpty(employee_id?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide an employee id.");
+                if (string.IsNullOrEmpty(firstName?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a first name.");
+                if (string.IsNullOrEmpty(lastName?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a last name.");
+                if (string.IsNullOrEmpty(phone_no?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a phone number.");
+                if (string.IsNullOrEmpty(address?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide an address.");
+                if (string.IsNullOrEmpty(social_insurance_no?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a social insurance number.");
+                if (string.IsNullOrEmpty(library_address?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a social insurance number.");
+
+                // Generate a new instance
+                Librarian_db instance = new Librarian_db
+                    (
+                        employee_id, firstName, lastName, phone_no, address, social_insurance_no, library_address
+                    );
+
+                // Add to database
+                int rowsAffected = context.ExecuteNonQueryCommand
+                    (
+                        commandText: "UPDATE customer SET First_name = @first_name, Last_name = @last_name, Password = @password, Address = @address, Date_of_birth = @date_of_birth WHERE card_id = @card_id",
+                        parameters: new Dictionary<string, object>()
+                        {
+                            {"@card_id", instance.Employee_id },
+                            { "@first_name", instance.First_name },
+                            { "@last_name", instance.Last_name },
+                            { "@password", instance.Phone_no },
+                            { "@address", instance.Address },
+                            { "@date_of_birth", instance.Social_insurance_no }
+                        },
+                        message: out string message
+                    );
+                if (rowsAffected == -1)
+                    throw new Exception(message);
+
+                // Return value
+                statusResponse = new StatusResponse("Customer edited successfully");
+                return instance;
+            }
+            catch (Exception exception)
+            {
+                statusResponse = new StatusResponse(exception);
+                return null;
+            }
+
+        }
+
+
+        /// <summary>
+        /// Deletes an instance in the database
+        /// </summary>
+        public static void Delete(int cardId, DbContext context, out StatusResponse statusResponse)
+        {
+            try
+            {
+                // Validate
+                if (cardId < 0)
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a card id.");
+
+
+                // Add to database
+                int rowsAffected = context.ExecuteNonQueryCommand
+                    (
+                        commandText: "DELETE FROM customer WHERE card_id = @card_id",
+                        parameters: new Dictionary<string, object>()
+                        {
+                            {"@card_id", cardId },
+                        },
+                        message: out string message
+                    );
+                if (rowsAffected == -1)
+                    throw new Exception(message);
+
+                // Return value
+                statusResponse = new StatusResponse("Customer deleted successfully");
+            }
+            catch (Exception exception)
+            {
+                statusResponse = new StatusResponse(exception);
+            }
+
+        }
+
+        /// <summary>
+        /// Retrieves an instance.
+        /// </summary>
+        public static Librarian_db Get(string employee_id,
+            DbContext context, out StatusResponse statusResponse)
+        {
+            try
+            {
+                // Get from database
+                DataTable table = context.ExecuteDataQueryCommand
+                    (
+                        commandText: "SELECT * FROM customer WHERE card_id = @card_id",
+                        parameters: new Dictionary<string, object>()
+                        {
+                            {"@employee_id", employee_id },
+                        },
+                        message: out string message
+                    );
+                if (table == null)
+                    throw new Exception(message);
+
+                DataRow row = table.Rows[0];
+
+                // Parse data
+                Librarian_db instance = new Librarian_db
+                            (
+                                employee_id: row["Employee_id"].ToString(),
+                                firstName: row["First_name"].ToString(),
+                                lastName: row["Last_name"].ToString(),
+                                phone_no: row["Phone_no"].ToString(),
+                                address: row["Address"].ToString(),
+                                social_insurance_no: row["Social_insurance_number"].ToString(),
+                                library_address: row["library_address"].ToString()
+                            );
+
+                // Return value
+                statusResponse = new StatusResponse("Librarian has been retrieved successfully.");
+                return instance;
+            }
+            catch (Exception exception)
+            {
+                statusResponse = new StatusResponse(exception);
+                return null;
+            }
+        }
+
+
+
+        /// <summary>
+        /// Retrieves a list of instances.
+        /// </summary>
+        public static List<Librarian_db> GetCollection(
+            DbContext context, out StatusResponse statusResponse)
+        {
+            try
+            {
+                // Get from database
+                DataTable table = context.ExecuteDataQueryCommand
+                    (
+                        commandText: "SELECT * FROM librarian",
+                        parameters: new Dictionary<string, object>()
+                        {
+
+                        },
+                        message: out string message
+                    );
+                if (table == null)
+                    throw new Exception(message);
+
+                // Parse data
+                List<Librarian_db> instances = new List<Librarian_db>();
+                foreach (DataRow row in table.Rows)
+                    instances.Add(new Librarian_db
+                            (
+                                employee_id: row["Employee_id"].ToString(),
+                                firstName: row["First_name"].ToString(),
+                                lastName: row["Last_name"].ToString(),
+                                phone_no: row["Phone_no"].ToString(),
+                                address: row["Address"].ToString(),
+                                social_insurance_no: row["Social_insurance_no"].ToString(),
+                                library_address: row["Library_address"].ToString()
+                            )
+                        );
+
+                // Return value
+                statusResponse = new StatusResponse("Customer list has been retrieved successfully.");
+                return instances;
+            }
+            catch (Exception exception)
+            {
+                statusResponse = new StatusResponse(exception);
+                return null;
+            }
+        }
+
+    }
+}
---
+++ Webservice/ControllerHelpers/LibrarianHelper.cs
@@ -0,0 +1,202 @@
+﻿using BusinessLibrary.Models;
+using DatabaseLibrary.Core;
+using DatabaseLibrary.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Webservice.ControllerHelpers
+{
+    public class LibrarianHelper
+    {
+
+        #region Converters
+
+        /// <summary>
+        /// Converts database models to a business logic object.
+        /// </summary>
+        public static BusinessLibrary.Models.Librarian Convert(Librarian_db instance)
+        {
+            if (instance == null)
+                return null;
+            return new BusinessLibrary.Models.Librarian(instance.Employee_id, instance.First_name, instance.Last_name, instance.Phone_no, instance.Address, instance.Social_insurance_no, instance.Library_address);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Signs up a librarian.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage Add(JObject data,
+            DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Extract paramters
+            string firstName = (data.ContainsKey("first_name")) ? data.GetValue("first_name").Value<string>() : null;
+            string lastName = (data.ContainsKey("last_name")) ? data.GetValue("last_name").Value<string>() : null;
+            string phone_no = (data.ContainsKey("phone_no")) ? data.GetValue("phone_no").Value<string>() : null;
+            string address = (data.ContainsKey("address")) ? data.GetValue("address").Value<string>() : null;
+            string social_insurance_no = (data.ContainsKey("social_insurance_no")) ? data.GetValue("social_insurance_no").Value<string>() : null;
+            string library_address = (data.ContainsKey("library_address")) ? data.GetValue("library_address").Value<string>() : null;
+
+
+            // Add instance to database
+            var dbInstance = DatabaseLibrary.Helpers.LibrarianHelper_db.Add("", firstName, lastName, phone_no, address, social_insurance_no, library_address,
+                context, out StatusResponse statusResponse);
+
+            // Get rid of detailed internal server error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors)
+                statusResponse.Message = "Something went wrong while adding a new librarian.";
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    dbInstance != null,
+                    statusResponse.Message,
+                    Convert(dbInstance)
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+        }
+
+        /// <summary>
+        /// Edits a librarian.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage Edit(JObject data,
+            DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Extract paramters
+            string employee_id = (data.ContainsKey("employee_id")) ? data.GetValue("employee_id").Value<string>() : null;
+            string firstName = (data.ContainsKey("first_name")) ? data.GetValue("first_name").Value<string>() : null;
+            string lastName = (data.ContainsKey("last_name")) ? data.GetValue("last_name").Value<string>() : null;
+            string phone_no = (data.ContainsKey("phone_no")) ? data.GetValue("phone_no").Value<string>() : null;
+            string address = (data.ContainsKey("address")) ? data.GetValue("address").Value<string>() : null;
+            string social_insurance_no = (data.ContainsKey("social_insurance_no")) ? data.GetValue("social_insurance_no").Value<string>() : null;
+            string library_address = (data.ContainsKey("library_address")) ? data.GetValue("library_address").Value<string>() : null;
+
+            // Add instance to database
+            var dbInstance = DatabaseLibrary.Helpers.LibrarianHelper_db.Edit(employee_id, firstName, lastName, phone_no, address, social_insurance_no, library_address,
+                context, out StatusResponse statusResponse);
+
+            // Get rid of detailed internal server error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors)
+                statusResponse.Message = "Something went wrong while editing a librarian.";
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    dbInstance != null,
+                    statusResponse.Message,
+                    Convert(dbInstance)
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+
+        }
+
+        /// <summary>
+        /// Deletes a librarian.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage Delete(JObject data,
+            DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Extract paramters
+            int employee_id = (data.ContainsKey("employee_id")) ? data.GetValue("employee_id").Value<int>() : -1;
+
+            // Add instance to database
+            DatabaseLibrary.Helpers.LibrarianHelper_db.Delete(employee_id, context, out StatusResponse statusResponse);
+
+            bool failed = false;
+
+            // Get rid of detailed internal server error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors) {
+                statusResponse.Message = "Something went wrong while deleting a librarian.";
+                failed = true;
+            }
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    failed,
+                    statusResponse.Message
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+        }
+
+
+        /// <summary>
+        /// Gets a librarian.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage Get(string? id,
+        DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Extract paramters
+            string employee_id = id;
+
+
+            // Get instances from database
+            var dbInstance = DatabaseLibrary.Helpers.LibrarianHelper_db.Get(employee_id,
+                context, out StatusResponse statusResponse);
+
+            // Convert to business logic objects
+            var instance = Convert(dbInstance);
+
+            // Get rid of detailed error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors)
+                statusResponse.Message = "Something went wrong while retrieving the librarian";
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    instance != null,
+                    statusResponse.Message,
+                    instance
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+        }
+
+
+        /// <summary>
+        /// Gets list of librarians.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage GetCollection(
+        DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Get instances from database
+            var dbInstances = DatabaseLibrary.Helpers.LibrarianHelper_db.GetCollection(
+                context, out StatusResponse statusResponse);
+
+            // Convert to business logic objects
+            var instances = dbInstances?.Select(x => Convert(x)).ToList();
+
+            // Get rid of detailed error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors)
+                statusResponse.Message = "Something went wrong while retrieving the librarians";
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    instances != null,
+                    statusResponse.Message,
+                    instances
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+        }
+
+    }
+}
