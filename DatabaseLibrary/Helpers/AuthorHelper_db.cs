0b33361b104416b21a0664e31864c934f26452a3
ParkerJSmith <parker.smith@shaw.ca>
2021-12-16 04:04:02 -0700

    Media

---
+++ DatabaseLibrary/Helpers/AuthorHelper_db.cs
@@ -0,0 +1,261 @@
+﻿using DatabaseLibrary.Core;
+using DatabaseLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Net;
+using System.Text;
+
+namespace DatabaseLibrary.Helpers
+{
+    public class AuthorHelper_db
+    {
+
+        /// <summary>
+        /// Adds a new instance into the database.
+        /// </summary>
+        public static Author_db Add(string author_id, string firstName, string lastName, DateTime dateOfBirth,
+            DbContext context, out StatusResponse statusResponse)
+        {
+            try
+            {
+                // Validate
+                if (string.IsNullOrEmpty(author_id?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a password.");
+                if (string.IsNullOrEmpty(firstName?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a first name.");
+                if (string.IsNullOrEmpty(lastName?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a last name.");
+                if (dateOfBirth == default(DateTime))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a date of birth.");
+
+                // Generate a new instance
+                Author_db instance = new Author_db
+                    (
+                        author_id, firstName, lastName, dateOfBirth
+                    );
+
+                // Add to database
+                int rowsAffected = context.ExecuteNonQueryCommand
+                    (
+                        commandText: "INSERT INTO Author (First_name, Last_name, Password, Address, Date_of_birth) values (@first_name, @last_name, @password, @address, @date_of_birth)",
+                        parameters: new Dictionary<string, object>()
+                        {
+                            { "@author_id", instance.Author_id },
+                            { "@first_name", instance.First_name },
+                            { "@last_name", instance.Last_name },
+                            { "@date_of_birth", instance.Date_of_birth }
+                        },
+                        message: out string message
+                    );
+                if (rowsAffected == -1)
+                    throw new Exception(message);
+
+                // Get from database
+                DataTable table = context.ExecuteDataQueryCommand
+                    (
+                        commandText: "SELECT LAST_INSERT_ID();",
+                        parameters: new Dictionary<string, object>()
+                        {
+
+                        },
+                        message: out string getMessage
+                    );
+                if (table == null)
+                    throw new Exception(getMessage);
+
+                DataRow row = table.Rows[0];
+
+                //instance.Card_id = Convert.ToInt32(row[0]);
+
+                // Return value
+                statusResponse = new StatusResponse("Author added successfully");
+                return instance;
+            }
+            catch (Exception exception)
+            {
+                statusResponse = new StatusResponse(exception);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Edits an instance in the database
+        /// </summary>
+        public static Author_db Edit(string author_id, string firstName, string lastName, DateTime dateOfBirth,
+           DbContext context, out StatusResponse statusResponse)
+        {
+            try
+            {
+                // Validate
+                if (string.IsNullOrEmpty(author_id?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a password.");
+                if (string.IsNullOrEmpty(firstName?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a first name.");
+                if (string.IsNullOrEmpty(lastName?.Trim()))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a last name.");
+                if (dateOfBirth == default(DateTime))
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide a date of birth.");
+
+                // Generate a new instance
+                Author_db instance = new Author_db
+                    (
+                        author_id, firstName, lastName, dateOfBirth
+                    );
+
+                // Add to database
+                int rowsAffected = context.ExecuteNonQueryCommand
+                    (
+                        commandText: "UPDATE Author SET First_name = @first_name, Last_name = @last_name, Password = @password, Address = @address, Date_of_birth = @date_of_birth WHERE card_id = @card_id",
+                        parameters: new Dictionary<string, object>()
+                        {
+                            {"@author_id", instance.Author_id },
+                            { "@first_name", instance.First_name },
+                            { "@last_name", instance.Last_name },
+                            { "@date_of_birth", instance.Date_of_birth }
+                        },
+                        message: out string message
+                    );
+                if (rowsAffected == -1)
+                    throw new Exception(message);
+
+                // Return value
+                statusResponse = new StatusResponse("Author edited successfully");
+                return instance;
+            }
+            catch (Exception exception)
+            {
+                statusResponse = new StatusResponse(exception);
+                return null;
+            }
+
+        }
+
+
+        /// <summary>
+        /// Deletes an instance in the database
+        /// </summary>
+        public static void Delete(string author_id, DbContext context, out StatusResponse statusResponse)
+        {
+            try
+            {
+                // Validate
+                if (author_id == "")
+                    throw new StatusException(HttpStatusCode.BadRequest, "Please provide an author id.");
+
+
+                // Add to database
+                int rowsAffected = context.ExecuteNonQueryCommand
+                    (
+                        commandText: "DELETE FROM Author WHERE author_id = @author_id",
+                        parameters: new Dictionary<string, object>()
+                        {
+                            {"@author_id", author_id },
+                        },
+                        message: out string message
+                    );
+                if (rowsAffected == -1)
+                    throw new Exception(message);
+
+                // Return value
+                statusResponse = new StatusResponse("Author deleted successfully");
+            }
+            catch (Exception exception)
+            {
+                statusResponse = new StatusResponse(exception);
+            }
+
+        }
+
+        /// <summary>
+        /// Retrieves an instance.
+        /// </summary>
+        public static Author_db Get(string author_id,
+            DbContext context, out StatusResponse statusResponse)
+        {
+            try
+            {
+                // Get from database
+                DataTable table = context.ExecuteDataQueryCommand
+                    (
+                        commandText: "SELECT * FROM Author WHERE author_id = @author_id",
+                        parameters: new Dictionary<string, object>()
+                        {
+                            {"@author_id", author_id },
+                        },
+                        message: out string message
+                    );
+                if (table == null)
+                    throw new Exception(message);
+
+                DataRow row = table.Rows[0];
+
+                // Parse data
+                Author_db instance = new Author_db
+                            (
+                                author_id: row["Author_id"].ToString(),
+                                firstName: row["First_name"].ToString(),
+                                lastName: row["Last_name"].ToString(),
+                                dateOfBirth: (DateTime)row["Date_of_birth"]
+                            );
+
+                // Return value
+                statusResponse = new StatusResponse("Author has been retrieved successfully.");
+                return instance;
+            }
+            catch (Exception exception)
+            {
+                statusResponse = new StatusResponse(exception);
+                return null;
+            }
+        }
+
+
+
+        /// <summary>
+        /// Retrieves a list of instances.
+        /// </summary>
+        public static List<Author_db> GetCollection(
+            DbContext context, out StatusResponse statusResponse)
+        {
+            try
+            {
+                // Get from database
+                DataTable table = context.ExecuteDataQueryCommand
+                    (
+                        commandText: "SELECT * FROM Author",
+                        parameters: new Dictionary<string, object>()
+                        {
+
+                        },
+                        message: out string message
+                    );
+                if (table == null)
+                    throw new Exception(message);
+
+                // Parse data
+                List<Author_db> instances = new List<Author_db>();
+                foreach (DataRow row in table.Rows)
+                    instances.Add(new Author_db
+                            (
+                                author_id: row["Author_id"].ToString(),
+                                firstName: row["First_name"].ToString(),
+                                lastName: row["Last_name"].ToString(),
+                                dateOfBirth: (DateTime)row["Date_of_birth"]
+                            )
+                        );
+
+                // Return value
+                statusResponse = new StatusResponse("Author list has been retrieved successfully.");
+                return instances;
+            }
+            catch (Exception exception)
+            {
+                statusResponse = new StatusResponse(exception);
+                return null;
+            }
+        }
+
+    }
+}
---
+++ Webservice/ControllerHelpers/LibraryHelper.cs
@@ -0,0 +1,197 @@
+﻿using BusinessLibrary.Models;
+using DatabaseLibrary.Core;
+using DatabaseLibrary.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Webservice.ControllerHelpers
+{
+    public class LibraryHelper
+    {
+
+        #region Converters
+
+        /// <summary>
+        /// Converts database models to a business logic object.
+        /// </summary>
+        public static BusinessLibrary.Models.Library Convert(Library_db instance)
+        {
+            if (instance == null)
+                return null;
+            return new BusinessLibrary.Models.Library(instance.Address, instance.Name, instance.Website_address, instance.Admin_id);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Signs up a Library.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage Add(JObject data,
+            DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Extract paramters
+            string address = (data.ContainsKey("address")) ? data.GetValue("address").Value<string>() : null;
+            string name = (data.ContainsKey("name")) ? data.GetValue("name").Value<string>() : null;
+            string website_address = (data.ContainsKey("website_address")) ? data.GetValue("website_address").Value<string>() : null;
+            string admin_id = (data.ContainsKey("admin_id")) ? data.GetValue("admin_id").Value<string>() : null;
+
+
+            // Add instance to database
+            var dbInstance = DatabaseLibrary.Helpers.LibraryHelper_db.Add("", name, website_address, admin_id,
+                context, out StatusResponse statusResponse);
+
+            // Get rid of detailed internal server error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors)
+                statusResponse.Message = "Something went wrong while adding a new Library.";
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    dbInstance != null,
+                    statusResponse.Message,
+                    Convert(dbInstance)
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+        }
+
+        /// <summary>
+        /// Edits a Library.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage Edit(JObject data,
+            DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Extract paramters
+            string address = (data.ContainsKey("address")) ? data.GetValue("address").Value<string>() : null;
+            string name = (data.ContainsKey("name")) ? data.GetValue("name").Value<string>() : null;
+            string website_address = (data.ContainsKey("website_address")) ? data.GetValue("website_address").Value<string>() : null;
+            string admin_id = (data.ContainsKey("admin_id")) ? data.GetValue("admin_id").Value<string>() : null;
+
+            // Add instance to database
+            var dbInstance = DatabaseLibrary.Helpers.LibraryHelper_db.Edit(address, name, website_address, admin_id,
+                context, out StatusResponse statusResponse);
+
+            // Get rid of detailed internal server error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors)
+                statusResponse.Message = "Something went wrong while editing a Library.";
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    dbInstance != null,
+                    statusResponse.Message,
+                    Convert(dbInstance)
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+
+        }
+
+        /// <summary>
+        /// Deletes a Library.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage Delete(JObject data,
+            DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Extract paramters
+            string address = (data.ContainsKey("address")) ? data.GetValue("address").Value<string>() : null;
+
+            // Add instance to database
+            DatabaseLibrary.Helpers.LibraryHelper_db.Delete(address, context, out StatusResponse statusResponse);
+
+            bool failed = false;
+
+            // Get rid of detailed internal server error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors) {
+                statusResponse.Message = "Something went wrong while deleting a Library.";
+                failed = true;
+            }
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    failed,
+                    statusResponse.Message
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+        }
+
+
+        /// <summary>
+        /// Gets a Library.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage Get(string? id,
+        DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Extract paramters
+            string address = id;
+
+
+            // Get instances from database
+            var dbInstance = DatabaseLibrary.Helpers.LibraryHelper_db.Get(address,
+                context, out StatusResponse statusResponse);
+
+            // Convert to business logic objects
+            var instance = Convert(dbInstance);
+
+            // Get rid of detailed error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors)
+                statusResponse.Message = "Something went wrong while retrieving the Library";
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    instance != null,
+                    statusResponse.Message,
+                    instance
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+        }
+
+
+        /// <summary>
+        /// Gets list of Libraries.
+        /// </summary>
+        /// <param name="includeDetailedErrors">States whether the internal server error message should be detailed or not.</param>
+        public static ResponseMessage GetCollection(
+        DbContext context, out HttpStatusCode statusCode, bool includeDetailedErrors = false)
+        {
+            // Get instances from database
+            var dbInstances = DatabaseLibrary.Helpers.LibraryHelper_db.GetCollection(
+                context, out StatusResponse statusResponse);
+
+            // Convert to business logic objects
+            var instances = dbInstances?.Select(x => Convert(x)).ToList();
+
+            // Get rid of detailed error message (when requested)
+            if (statusResponse.StatusCode == HttpStatusCode.InternalServerError
+                && !includeDetailedErrors)
+                statusResponse.Message = "Something went wrong while retrieving the Libraries";
+
+            // Return response
+            var response = new ResponseMessage
+                (
+                    instances != null,
+                    statusResponse.Message,
+                    instances
+                );
+            statusCode = statusResponse.StatusCode;
+            return response;
+        }
+
+    }
+}
